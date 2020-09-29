6ccfb453130c8367a1a837f46fb42337c38a032c
yassine <yassine.chafyaay@ibm.com>
2020-09-29 15:33:40 +0100

    connecte store

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todo from "./components/todo";
+import axios from "axios";
+
+export default class App extends Component {
+  state = {
+    token: "",
+    kpi: ""
+  };
+  componentDidMount() {
+    this.getToken();
+  }
+
+  getToken = async () => {
+    try {
+      let res = await axios.get('http://localhost:7777');
+      // let header = res.data;
+
+      this.setState(
+        {
+          token: res.data.csrfToken,
+          kpi: res.data["x-gateway-apikey"],
+        }
+      );
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (<div className="App">
+
+      <h1>{this.state.token}</h1>
+
+      <Todo kpi={this.state.kpi} token={this.state.token}></Todo>
+    </div>);
+  }
+}
+
+
+
